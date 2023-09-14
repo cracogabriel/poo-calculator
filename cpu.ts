885a3b935491e1258653afc9b4dfc45fedc08452
gabriel craco tasarz <87382334+cracogabriel@users.noreply.github.com>
2023-09-14 10:04:39 -0300

    refactor(cpu): remove eval

--- cpu.ts
+++ cpu.ts
@@ -1,14 +1,53 @@
 class Cpu {
   calcularExpressao(expressao: string): number {
     try {
-      const resultado = eval(expressao);
-      if (isNaN(resultado)) {
-        throw new Error("Expressão inválida");
-      }
+      const resultado = this.analisarExpressao(expressao);
       return resultado;
     } catch (error: any) {
       throw new Error("Erro ao calcular a expressão: " + error.message);
     }
+  }
+
+  private analisarExpressao(expressao: string): number {
+    let resultado = 0;
+    let operador = "+";
+    let numero = "";
+
+    for (let i = 0; i < expressao.length; i++) {
+      const char = expressao.charAt(i);
+
+      if (/[0-9]/.test(char)) {
+        numero += char;
+      } else if (/[+\-*/]/.test(char)) {
+        operador = char;
+      } else if (char === " ") {
+        continue;
+      } else {
+        throw new Error("Caractere inválido na expressão");
+      }
+
+      if (/[+\-*/]/.test(char) || i === expressao.length - 1) {
+        const valor = parseInt(numero);
+        if (isNaN(valor)) {
+          throw new Error("Número inválido na expressão");
+        }
+
+        if (operador === "+") {
+          resultado += valor;
+        } else if (operador === "-") {
+          resultado -= valor;
+        } else if (operador === "*") {
+          resultado *= valor;
+        } else if (operador === "/") {
+          resultado /= valor;
+        }
+
+        operador = "+";
+        numero = "";
+      }
+    }
+
+    return resultado;
   }
 }
 
