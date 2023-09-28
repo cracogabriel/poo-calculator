04149255e92f25833cb4a3e1d61b9076f80e6878
gabriel craco tasarz <87382334+cracogabriel@users.noreply.github.com>
2023-09-28 16:02:01 -0300

    feat(utils): add enum for symbols

---
+++ utils/Enums.ts
@@ -0,0 +1,24 @@
+export enum NumbersEnum {
+  ZERO = "0",
+  ONE = "1",
+  TWO = "2",
+  THREE = "3",
+  FOUR = "4",
+  FIVE = "5",
+  SIX = "6",
+  SEVEN = "7",
+  EIGHT = "8",
+  NINE = "9",
+}
+
+export enum OperatorsEnum {
+  TIMES = "*",
+  DIVISION = "/",
+  SUM = "+",
+  SUBTRACTION = "-",
+}
+
+export enum ControlEnum {
+  EQUAL = "=",
+  CLEAR = "C",
+}
